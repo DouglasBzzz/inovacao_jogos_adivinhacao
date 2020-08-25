558eb9e345941b9715ae7743566acfdc04ac95c4
DouglasBzzz <d.bianchezzi@gmail.com>
2020-08-24 21:27:34 -0300

    adicionando arquivos de jogos ao repositório

---
+++ forca.py
@@ -0,0 +1,24 @@
+import random
+
+def jogar():
+    print("Bem vindo a Forca!")
+
+    arquivo = open("palavras.txt","r")
+    palavras = []
+
+    for linha in arquivo:
+        palavras.append(linha.strip())
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+
+    palavra_objetivo = palavras[numero]
+    print(palavra_objetivo)
+
+    letras_encontradas = ["_" for letra in palavra_objetivo]
+    print(letras_encontradas)
+
+    print("END GAME")
+
+if(__name__ == "__main__"):
+    jogar()
